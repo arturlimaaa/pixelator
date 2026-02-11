d4cb771a0c061ad5b4131a8ba4233ec945bfaef6
arturlimaaa <arturlimaj04@gmail.com>
2026-02-11 15:37:49 -0500

    Change base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/projects/pixelator/',
+  base: '/projects/pixelator-app/',
   plugins: [react()],
 })
